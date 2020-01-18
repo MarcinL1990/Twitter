2548e8afaee211e71c147a774f02f116b893f821
MarcinL1990 <marcinl1990@o2.pl>
2020-01-18 14:27:22 +0100

    Initial commit. User model, dao, service created

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.User;
+import java.util.Set;
+
+public interface UserDAO {
+    void saveUser(User user);
+    void deleteUser(Long userId);
+    User getUserByLogin(String login);
+    User getUserByEmail(String email);
+    Set<User> getFollows(String login);
+    Set<User> getFollowers(String login);
+    Set<User> getNotFollowedUsers(String login);
+}
---
+++ src/main/java/services/UserManagementService.java
@@ -0,0 +1,14 @@
+package services;
+
+import model.User;
+import java.util.Set;
+
+public interface UserManagementService {
+    void saveUser(User user);
+    void deleteUser(Long userId);
+    void follow(String currentUserLogin, String userLoginToFollow);
+    void stopFollowing(String currentUserLogin, String userLoginToUnfollow);
+    Set<User> getNotFollowedUsers(String login);
+    boolean isUserValid(String login, String password);
+    boolean isUserExists(String login);
+}
